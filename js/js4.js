bfe191795028552e6a875e2e9e0d835dce6cdb0a
shuyin21 <shuyindev@gmail.com>
2022-05-12 15:28:59 +0100

    calculator made

--- js/js4.js
+++ js/js4.js
@@ -2,6 +2,11 @@ const arrayDiv = document.getElementById('array-div');
 const first = document.getElementById('fNum');
 const second = document.getElementById('sNum');
 const answer = document.getElementById('answer');
+const display = document.getElementById('display');
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const selection = document.getElementById('selection');
+
 
 const fruits = ["apple", "banana", "pear", "grapes", "cherries", "plum", "pineapple"];
 
@@ -25,4 +30,30 @@ const till = () => {
     first.value = '';
     second.value = '';
     console.log(first);
+}
+
+const calculate = () => {
+    let sel = selection.value;
+    let n1 = Number(num1.value);
+    let n2 = Number(num2.value);
+    let sum = ''
+    if (sel === '+') {
+        sum = n1 + n2;
+    }
+    else if (sel === '-') {
+        sum = n1 - n2;
+    }
+    else if (sel === '*') {
+        sum = n1 * n2;
+    }
+    else if (sel === '/') {
+        sum = n1 / n2;
+    }
+    else if (sel === '%') {
+        sum = n1 % n2;
+    }
+
+    display.innerHTML = n1 + sel + n2 + '=' + ' ' + sum;
+    num1.value = '';
+    num2.value = '';
 }
