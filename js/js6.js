84c9a1035f622ffb136527af886b3e85c0ffe667
shuyin21 <shuyindev@gmail.com>
2022-05-16 16:45:38 +0100

    finished

--- js/js6.js
+++ js/js6.js
@@ -1,3 +1,18 @@
-console.log(document.querySelector('.highlight i').innerText);
-const play = document.querySelector('.highlight');
-console.log(play);
+const box = document.querySelector('.small-box');
+const btn = document.querySelector('button');
+const magic = document.querySelector('#magic h3');
+
+box.addEventListener('mouseenter', () => {
+
+    box.classList.add('extra-size');
+});
+box.addEventListener('mouseleave', () => {
+
+    box.classList.remove('extra-size');
+});
+
+btn.addEventListener('click', () => {
+
+    magic.classList.toggle('hide');
+
+})
